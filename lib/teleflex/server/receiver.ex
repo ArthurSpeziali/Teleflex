44809d8a23a3bde50dec0f9c2de3c0760c7fb661
ArthurSpeziali <arthur.speziali2022@gmail.com>
2026-09-06 22:56:45 -0300

    Now, Receiver is a trully GenServer

--- lib/teleflex/server/receiver.ex
+++ lib/teleflex/server/receiver.ex
@@ -1,19 +1,29 @@
 defmodule Teleflex.Server.Receiver do 
+  use GenServer
   alias Teleflex.Contract
   @node_proc Application.compile_env(:teleflex, :node_opts)[:proc]
 
-  def start_link(_state \\ nil) do 
+  # GenServer functions 
+  def start_link(state \\ []) do 
     if Process.whereis(@node_proc) do 
       {:error, "server already started"}
     else
-      pid = spawn(&receiver/0)
-      Process.register(pid, @node_proc)
-
-      {:ok, pid}
+      GenServer.start_link(__MODULE__, state, name: __MODULE__) 
     end
   end
+  
+  def init(state) do 
+    spawn_monitor(&receiver/0)
 
-  # Functions 
+    {:ok, state}
+  end 
+
+  def handle_info({:DOWN, _ref, :process, _pid, :normal}, state) do 
+    {:stop, :normal, state}
+  end
+
+
+  # API functions 
   def receiver() do 
     receive do 
       %Contract{} = contract -> 
